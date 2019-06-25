0a737c377326490acec54dc34c30d616e35e7583
Guilherme Portugues <g_portugues@hotmail.com>
2019-06-25 17:15:45 -0300

    Alterada a geração do arquivo initilizer

---
+++ lib/generators/keycloak/config/config_generator.rb
@@ -0,0 +1,10 @@
+module Keycloak
+  module Generators
+    class ConfigGenerator < Rails::Generators::Base
+      source_root(File.expand_path(File.dirname(__FILE__)))
+      def copy_initializer
+        copy_file 'keycloak.rb', 'config/initializers/keycloak.rb'
+      end
+    end
+  end
+end
--- lib/generators/keycloak_generator.rb
+++
@@ -1,8 +0,0 @@
-class KeycloakGenerator < Rails::Generators::Base
-	source_root(File.expand_path(File.dirname(__FILE__)))
-
-	def copy_initializer
-		copy_file 'keycloak.rb', 'config/initializers/keycloak.rb'
-	end
-
-end
