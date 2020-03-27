f0151a2069829f6bb4786d79117d9fe080aab4b2
Alvaro Gomez Traveso <agtraveso.developer@gmail.com>
2020-03-27 18:55:09 +0100

    Complete rspecs

--- spec/keycloak_spec.rb
+++ spec/keycloak_spec.rb
@@ -29,12 +29,15 @@ RSpec.describe Keycloak do
       before do
         @a_realm = 'irrelevant_realm'
         @auth_server_url = 'irrelevant_url'
+        @a_resource = 'irrelevant_resource'
+        @a_secret = 'irrelevant_secret'
+        @a_public_key = 'irrelevant_public_key'
         @properties = {
           realm: @a_realm,
           auth_server_url: @auth_server_url,
-          client_id: 'irrelevant_client_id',
-          credentials: { secret: 'irrelevant_secret' },
-          realm_public_key: 'irrelevant_public_key'
+          resource: @a_resource,
+          credentials: { secret: @a_secret },
+          realm_public_key: @a_public_key
         }
       end
 
@@ -43,6 +46,9 @@ RSpec.describe Keycloak do
 
         expect(@client.realm).to eq(@a_realm)
         expect(@client.auth_server_url).to eq(@auth_server_url)
+        expect(@client.client_id).to eq(@a_resource)
+        expect(@client.secret).to eq(@a_secret)
+        expect(@client.public_key).to eq(@a_public_key)
       end
 
       it 'should initialize without properties' do
