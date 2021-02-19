8493ca05e16d581a3076ba3b41e38bd3edc05f80
Ale Ornelas <afigueroa@goamv.com>
2021-02-18 18:59:23 -0600

    Only logout when refresh token is given

--- lib/keycloak.rb
+++ lib/keycloak.rb
@@ -925,7 +925,7 @@ module Keycloak
 
           Keycloak::Client.exec_request _request
         ensure
-          if tk
+          if tk and tk["refresh_token"].present?
             payload = { 'client_id' => client_id,
                         'client_secret' => secret,
                         'refresh_token' => tk["refresh_token"] }
